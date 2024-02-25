e0cc482de01b8f14f7d0c9f4d11807207273996e
Janish-1 <janish.pancholi11@gmail.com>
2024-02-25 17:55:34 +0530

    remioved react app and added a template

--- maincrm/maincrm/settings.py
+++ maincrm/maincrm/settings.py
@@ -127,6 +127,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Linking the css files
+STATICFILES_DIRs = [
+    BASE_DIR / 'static/'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
